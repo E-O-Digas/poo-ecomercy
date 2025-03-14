61ac169efc0349db0477e659d6616b3d83d4bed0
E-O-Digas <guitoempireallias@gmail.com>
2025-03-14 16:12:33 -0300

    Aula01: Setup de middlewares e controllers + criação de pastas usando modelo MVC

--- Program.cs
+++ Program.cs
@@ -5,8 +5,6 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// app.MapGet("/", () => "AHHHHHHHHHHHHHHHHHHHHHHHhh");
-
 app.MapControllerRoute("deafult", "/{controller}/{action}");
 
 app.Run();
