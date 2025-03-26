d598c3c31f17666fa29ec09286daedfed5a41560
E-O-Digas <guitoempireallias@gmail.com>
2025-03-26 15:44:52 -0300

    tabela de produtos

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,10 +1,17 @@
+using Ecommerce.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.Controllers;
 
 public class ProdutoController : Controller {
     public ActionResult Index() {
-        return View();
+        List<Produto> produtos = new List<Produto>();
+
+        produtos.Add(new Produto {ProdutoId = 1, Nome = "A"});
+        produtos.Add(new Produto {ProdutoId = 2, Nome = "B"});
+        produtos.Add(new Produto {ProdutoId = 3, Nome = "C"});
+
+        return View(produtos);
 
     }
 
